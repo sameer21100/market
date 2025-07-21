dd6f3b969870162fa352524484c85f0710cd865f
Sameer Yadav <sydv21102000@gmail.com>
2025-07-21 15:55:06 +0530

    basic config

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+@app.route("/")
+def main():
+    return render_template("home.html")
+@app.route("/market")
+def market():
+    return render_template("market.html")
